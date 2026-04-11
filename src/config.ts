1d8c66d3c2c0c1f63707a5b055dd94e4061ba908
ganyvze <18938960129fish@gmail.com>
2026-04-11 18:01:58 +0800

    add ganyvze.github.io and oirsh.cn to the navigation links

--- src/config.ts
+++ src/config.ts
@@ -44,11 +44,16 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-//		{
-//			name: "GitHub",
-//			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-//			external: true, // Show an external link icon and will open in a new tab
-//		},
+		{
+			name: "ganyvze.github.io",
+			url: "https://ganyvze.github.io", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+		{
+			name: "oirsh.cn",
+			url: "https://oirsh.cn",
+			external: true,
+		},
 	],
 };
 
